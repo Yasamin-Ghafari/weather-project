70cb8710da8ab0f4875a6fee3f667e1cb41ccdd6
yasi <nahidghafari1831@gmail.com>
2024-10-24 00:56:13 -0700

    implement weather component

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,8 +1,25 @@
 import Searchinfo from "@/components/Searchinfo";
 import Weatherinfo from "@/components/Weatherinfo";
 import Forecastlist from "@/components/Forecastlist";
+import {useState} from "react";
 
-function Weather() {
+interface Props {
+    city: string;
+}
+function Weather({city}:Props) {
+    const [WeatherState , SetWeatherState] = useState<Weather>({
+        city : city,
+        wind : '',
+        humidity : '',
+        description : '',
+        icon : '',
+        daily : [],
+    })
+    // console.log(WeatherState);
+
+    // async function CallWeatherApi = () : void =>{
+    //     await fetch() ;
+    // }
     return (
         <div>
             <Searchinfo/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
 
       <main>
-          <Weather/>
+          <Weather city={"Tehran"}/>
       </main>
 
   )
